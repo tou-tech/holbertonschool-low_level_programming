7be66fa8ba656c6a5c8f9d0f5f3952e7370f4613
tou-tech <toulout.asmae@gmail.com>
2026-02-25 13:08:58 +0100

    1-main.c

--- functions_nested_loops/1-main.c
+++ functions_nested_loops/1-main.c
@@ -1,8 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-        print_alphabet();
-        return (0);
+    print_alphabet();
+    return (0);
 }

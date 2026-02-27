47f57c0a6a3e2e333010676b39594a4792c14f4f
tou-tech <toulout.asmae@gmail.com>
2026-02-27 12:24:51 +0100

    Add all prototypes and fix print_most_numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14,
+ * followed by a new line.
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+int i, j;
+
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+if (j > 9)
+{
+_putchar((j / 10) + '0'); /* Premier _putchar (pour le '1' de 10-14) */
+			}
+_putchar((j % 10) + '0'); /* Deuxième _putchar (pour l'unité) */
+}
+_putchar('\n'); /* Troisième _putchar (saut de ligne) */
+}
+}
